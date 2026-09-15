a3673688bb6cf29b0d72c89890f52556921ff96f
beannnie <novelsb72@naver.com>
2026-09-15 16:28:29 +0900

    Feat: 메모 타입 및 샘플 데이터 정의

---
+++ src/data/memoList.ts
@@ -0,0 +1,14 @@
+import type { Memo } from '../types/memo'
+
+export const initialMemoList: Memo[] = [
+    { id: 1, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Daily', date: '2026-09-15', isPinned: true },
+    { id: 2, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Work', date: '2026-09-15', isPinned: true },
+    { id: 3, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Others', date: '2026-09-16', isPinned: false },
+    { id: 4, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Daily', date: '2026-09-16', isPinned: false },
+    { id: 5, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Work', date: '2026-09-17', isPinned: false },
+    { id: 6, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Others', date: '2026-09-17', isPinned: false },
+    { id: 7, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Daily', date: '2026-09-18', isPinned: false },
+    { id: 8, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Work', date: '2026-09-18', isPinned: false },
+    { id: 9, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Others', date: '2026-09-19', isPinned: false },
+    { id: 10, title: '이것은 제목입니다', content: '이것은 본문입니다 이것은 본문입니다 이것은 본문입니다', category: 'Daily', date: '2026-09-19', isPinned: false },
+]
